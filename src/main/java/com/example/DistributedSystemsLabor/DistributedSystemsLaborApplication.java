43ccceded8d48e5e61793d9cb2768c530056b255
Gerry Wildermuth <gerry1313@web.de>
2018-11-05 15:07:47 +0100

    Changed database to H2 hopefully will stay persist from now on. Added new initialise to Airplane Controller.

--- src/main/java/com/example/DistributedSystemsLabor/DistributedSystemsLaborApplication.java
+++ src/main/java/com/example/DistributedSystemsLabor/DistributedSystemsLaborApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DistributedSystemsLaborApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DistributedSystemsLaborApplication.class, args);
-
-	}
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DistributedSystemsLaborApplication.class, args);
+    }
 }
