837ace66d113fb904f0cb7a5fbef8afef8d621ef
Firat Susan <firat.susan@gmail.com>
2018-11-24 18:03:18 +0100

    Added CORS Support

--- src/main/java/com/example/DistributedSystemsLabor/DistributedSystemsLaborApplication.java
+++ src/main/java/com/example/DistributedSystemsLabor/DistributedSystemsLaborApplication.java
@@ -2,6 +2,12 @@ package com.example.DistributedSystemsLabor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class DistributedSystemsLaborApplication {
@@ -9,5 +15,15 @@ public class DistributedSystemsLaborApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DistributedSystemsLaborApplication.class, args);
 
+	}
+
+	@Configuration
+	@EnableWebMvc
+	public class WebConfig implements WebMvcConfigurer {
+
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**");
+		}
 	}
 }
